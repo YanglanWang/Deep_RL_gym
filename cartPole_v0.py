81a63c1048c47bdd9fb089e8f501350f78ee554d
yanglan <yanglan-17@mails.tsinghua.edu.cn>
2019-04-20 17:31:58 +0800

    4.20

---
+++ cartPole_v0.py
@@ -0,0 +1,29 @@
+#
+# import gym
+# env = gym.make('CartPole-v0')
+# env.reset()
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+# env.close()
+#
+#
+# import gym
+# env = gym.make('CartPole-v0')
+# for i_episode in range(20):
+#     observation = env.reset()
+#     for t in range(100):
+#         env.render()
+#         print(observation)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             breakjupyter notebook
+# env.close()
+
+from gym import spaces
+space = spaces.Discrete(8) # Set with 8 elements {0, 1, 2, ..., 7}
+x = space.sample()
+assert space.contains(x)
+assert space.n == 8
